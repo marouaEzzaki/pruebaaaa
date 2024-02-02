5d86ccbc9f57e0a902486a4f00b0a80111466802
yassine <yassine@marwas-mbp.home>
2024-02-02 23:43:18 +0100

    modificacion depsues de push

--- probandoGit_sin_apuntes/src/probandoGit_sin_apuntesPAQUETE/primeraClase_respaldada.java
+++ probandoGit_sin_apuntes/src/probandoGit_sin_apuntesPAQUETE/primeraClase_respaldada.java
@@ -4,7 +4,10 @@ public class primeraClase_respaldada {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba 1");
-
+		
+		System.out.println("Prueba 2 (commit)"
+				+ "vamos a ver si se actualiza en el remoto");
+		
 		
 		
 	}
